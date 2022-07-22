0f2b8bb5630ac9cb0ac0fdae889d20166ea82ab0
Jun <56570380+Jiby-HJ@users.noreply.github.com>
2022-07-22 15:33:42 +0900

    Create_Geofence

--- app/src/main/java/com/jun/geofence/MainActivity.kt
+++ app/src/main/java/com/jun/geofence/MainActivity.kt
@@ -3,7 +3,9 @@ package com.jun.geofence
 import android.Manifest
 import android.annotation.SuppressLint
 import android.app.AlertDialog
+import android.app.PendingIntent
 import android.content.DialogInterface
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Build
@@ -17,10 +19,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.Geofence
-import com.google.android.gms.location.GeofencingClient
-import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -220,7 +219,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
 
-    fun createGeoFence(latlng: LatLng,radius:Float) {
+    private fun createGeoFence(latlng: LatLng, radius:Float) {
         val geofenceList = arrayListOf<Geofence>()
         geofenceList.add(
             Geofence.Builder()
@@ -242,6 +241,30 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         callbacks?.geofenceRequest(geofenceList)
     }
 
+     fun geofenceRequest(arrayList: ArrayList<Geofence>) {
+        val request = GeofencingRequest.Builder().apply {
+            setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER)
+            addGeofences(arrayList)
+        }.build() // build the GeoFenceRequest
+
+        val intent = Intent(this, GeofenceReceiver::class.java)
+        val pendingIntent =
+            PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) return
+        geofencingClient.addGeofences(request, pendingIntent).run {
+            addOnSuccessListener {
+                printShortToast(getString(R.string.geofence_enabled))
+            }
+            addOnFailureListener {
+                printShortToast(getString(R.string.geofence_error))
+            }
+        }
+    }
 
     /**
      * Gets the current location of the device, and positions the map's camera.
